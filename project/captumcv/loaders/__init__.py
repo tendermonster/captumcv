196de8e4237ea49376028ba4859abab1da7274d1
Artiom Blinovas <25306286+tendermonster@users.noreply.github.com>
2023-05-21 13:40:26 +0200

    removing uneeded files, gitignore, implementing funcioning model loader example

--- project/captumcv/loaders/__init__.py
+++ project/captumcv/loaders/__init__.py
@@ -1,13 +1,1 @@
 # just a placeholder for python
-import torch
-
-class Loader:
-    def __init__(self, model_path):
-        self.device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-        self.model = torch.load(model_path, map_location=self.device)
-        self.model.eval()
-
-    def predict(self, input_tensor):
-        with torch.no_grad():
-            output = self.model(input_tensor.to(self.device))
-        return output
