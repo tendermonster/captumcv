1c524fa109aecdf57f06cc3d48166b03c872adb3
Babar <arctanx@arctanx.speedport.ip>
2023-07-02 22:39:58 +0200

    reimplement plot for GradCam into code

--- project/test_GradCam.py
+++ project/test_GradCam.py
@@ -9,8 +9,13 @@ from captum.attr import visualization as viz
 from matplotlib import pyplot as plt
 
 
-model_path = os.path.join("captumcv", "model_weights", "SimpleDLA_10epochs_cifar10.pth")
-img = Image.open(os.path.join("resources", "testbild.jpg"))
+model_path = os.path.join(
+    "captumcv", "model_weights", "SimpleDLA_10epochs_cifar10.pth"
+)
+
+img = Image.open(
+    os.path.join("resources", "testbild.jpg")
+)  
 
 model_loader = DLASimpleLoader(model_path)
 model = model_loader.model.module.to("cpu")
@@ -25,6 +30,11 @@ attribution_np = np.transpose(
 img = np.array(img)
 print(attribution.shape)
 
+# Visualize Guided Grad-Cam
+#heatmap = attribution.squeeze().detach().numpy().to
+#heatmap = cv2.applyColorMap((heatmap*255).astype(np.uint8), cv2.COLORMAP_JET)
+#result = heatmap*0.3 + input_image*0.5
+
 f, ax = viz.visualize_image_attr_multiple(
     attribution_np,
     img,
@@ -35,8 +45,4 @@ f, ax = viz.visualize_image_attr_multiple(
 )
 
 plt.show(f)
-heatmap = attribution.squeeze()
-heatmap = heatmap.detach().numpy()
-heatmap = cv2.applyColorMap((heatmap * 255).astype(np.uint8), cv2.COLORMAP_JET)
-
-result = heatmap * 0.3 + input_image * 0.5
+print(attribution)
